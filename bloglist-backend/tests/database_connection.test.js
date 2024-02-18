ed0d8a05f4030d1071da069eac3daf6b54af1c57
Catrovitch <ale.engelhardt@outlook.com>
2024-02-18 11:41:04 +0200

    testing pipeline

--- bloglist-backend/tests/database_connection.test.js
+++ bloglist-backend/tests/database_connection.test.js
@@ -3,14 +3,19 @@ const config = require('../utils/config');
 
 // Function to check if MONGODB_URI is defined and not empty
 const isMongoDBUriValid = () => {
-    return config.MONGODB_URI.length > 0;
-  };
-  
+  const valid = config.MONGODB_URI && config.MONGODB_URI.length > 0;
+  console.log('Length of MONGODB_URI is not zero: ', valid);
+  if (config.MONGODB_URI === "MONGODB_URI_HARDCODED") {
+    console.log('Mongo db uri: MONGODB_URI_HARDCODED');
+  }
+  return valid;
+};
 
 describe('Database Connection Test', () => {
   it('should log the value of NODE_ENV and test database connection', async () => {
+    
     console.log('NODE_ENV:', process.env.NODE_ENV);
-    console.log('Length of MONGODB_URI is not zero: ', isMongoDBUriValid())
+    isMongoDBUriValid()
 
     // Establish a connection to the MongoDB database
     await mongoose.connect(config.MONGODB_URI, {
