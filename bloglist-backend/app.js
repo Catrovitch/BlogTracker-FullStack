1aff8d15143f3711c5935c77a62766cb3aca4ddf
Catrovitch <ale.engelhardt@outlook.com>
2024-02-18 21:52:52 +0200

    setting up deployment

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -27,12 +27,12 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
-app.use('/api/users/', usersRouter)
+app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 if (process.env.NODE_ENV === 'test') {
--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -4,5 +4,4 @@ const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
-  console.log(`Server running on port ${config.PORT}`)
 })
