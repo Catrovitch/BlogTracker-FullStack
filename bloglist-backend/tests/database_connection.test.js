85f62211db348f896e353e57e7678b3fa6fe02e8
Catrovitch <ale.engelhardt@outlook.com>
2024-02-18 12:11:56 +0200

    adding check for existing connection

--- bloglist-backend/tests/database_connection.test.js
+++ bloglist-backend/tests/database_connection.test.js
@@ -14,23 +14,28 @@ const isMongoDBUriValid = () => {
 };
 
 describe('Database Connection Test', () => {
-  it('should log the value of NODE_ENV and test database connection', async () => {
-    
-    console.log('NODE_ENV:', process.env.NODE_ENV);
-    isMongoDBUriValid()
-
-    // Establish a connection to the MongoDB database
-    await mongoose.connect(config.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }, 10000);
-
-    try {
-      // Check if the connection to the database is successful
-      expect(mongoose.connection.readyState).toBe(1); // 1 indicates that the connection is open
-    } finally {
-      // Close the connection after the test
-      await mongoose.connection.close();
-    }
+    it('should log the value of NODE_ENV and test database connection', async () => {
+      console.log('NODE_ENV:', process.env.NODE_ENV);
+      isMongoDBUriValid();
+  
+      // Check if already connected
+      if (mongoose.connection.readyState === 1) {
+        console.log('Already connected to the database.');
+      } else {
+        // Establish a connection to the MongoDB database
+        await mongoose.connect(config.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }, 10000);
+      }
+  
+      try {
+        // Check if the connection to the database is successful
+        expect(mongoose.connection.readyState).toBe(1); // 1 indicates that the connection is open
+      } finally {
+        // Close the connection after the test
+        await mongoose.connection.close();
+      }
+    });
   });
-});
+  
